3942d07ac36e70dea66eeb15106afb2606c52123
KP <jkp.dev@mail.com>
2025-02-23 23:17:54 +0530

    all code

--- jeyam/src/app/app.component.ts
+++ jeyam/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'jeyam';
 }
